5e3cc1c560ffbebe4c38d8c8122b27971657f5e4
maryam.mousazade <maryam.mousazade>
2025-01-10 18:58:57 +0330

    adding database

--- main.go
+++ main.go
@@ -1,32 +1,45 @@
 package main
 
 import (
+	"log"
 	"net/http"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 
 	"github.com/gin-gonic/gin"
 )
 
 type Book struct {
-	ID     string `json:"id"`
+	ID     string `json:"id" gorm:"primaryKey"`
 	Title  string `json:"title"`
 	Author string `json:"author"`
 	Year   string `json:"year"`
 }
 
-var books = []Book{
-	{
-		ID:     "1",
-		Title:  "GO PROGRAMMING",
-		Author: "John",
-		Year:   "2021"},
-	{
-		ID:     "2",
-		Title:  "Gin",
-		Author: "Jane",
-		Year:   "2022"},
-}
+
+//تعریف متغیر يایگاه داده
+
+var db *gorm.DB
 
 func main() {
+	//اتصال به دیتابیس
+
+	var err error
+	db, err = gorm.Open(sqlite.Open("books.db"), &gorm.Config{})
+	iferr != nil {
+		log.Fatal("faild to connect to database:", err)
+	}
+	 //مهاجرت دیتابیس
+	 if err := db.AutoMigrate(&Book{});
+	 err != nil {
+		log.Fatal("faild to migrete database:", err)
+	 }
+
+	 //داده نمونه
+
+	 initSampleData()
+
 	r := gin.Default()
 
 	r.GET("/books", getBooks)
@@ -38,20 +51,46 @@ func main() {
 	r.Run(":8080")
 }
 
+func initSampleData() {
+	sampleBooks := []Book{
+		{
+			ID:     "1",
+			Title:  "GO PROGRAMMING",
+			Author: "John",
+			Year:   "2021"},
+		{
+			ID:     "2",
+			Title:  "Gin",
+			Author: "Jane",
+			Year:   "2022"},
+	}
+	for _, book := range sampleBooks {
+		var existing Book
+		if err := db.First(&existing, "id=?", book.ID).Error; err != nil {
+			db.Create(&book)
+		}
+	}
+}
+
 func getBooks(c *gin.Context) {
+	var books []Book
+	if err := db.Find(&books).Error; err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message" : "failed to retrieve books"})
+		return
+	}
 	c.IndentedJSON(http.StatusOK, books)
 }
 
 func getBookByID(c *gin.Context) {
 	id := c.Param("id")
+	var book Book
 
-	for _, book := range books {
-		if book.ID == id {
-			c.IndentedJSON(http.StatusOK, book)
-			return
-		}
-	}
-	c.IndentedJSON(http.StatusFound, gin.H{"message": "Book not found"})
+if err := db.First(&existing, "id=?", book.ID).Error; err != nil {
+	c.IndentedJSON(http.StatusInternalServerError, gin.H{"message" : "book not found"})
+		return
+
+}
+c.IndentedJSON(http.StatusOK, books)
 }
 
 func createBook(c *gin.Context) {
@@ -62,7 +101,12 @@ func createBook(c *gin.Context) {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{"message": "invalid data"})
 		return
 	}
-	books = append(books, newBook)
+	if err := db.Create(&newBook).Error;
+	err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message": "faild to creat book"})
+		return
+	}
+	
 	c.IndentedJSON(http.StatusCreated, newBook)
 }
 
@@ -76,39 +120,31 @@ func updateBook(c *gin.Context) {
 		return
 
 	}
-
-	for i, book := range books {
-		if book.ID == id {
-			books[i] = updatedBook
-
-			c.IndentedJSON(http.StatusOK, updatedBook)
-			return
-		}
+	var book Book
+	if err := db.First(&book, "id = ?" , id).Error; err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message" : :"book not found"})
+		return
 	}
-	c.IndentedJSON(http.StatusFound, gin.H{"message": "Book not found"})
+
+	book.Title = updatedBook.Title
+	book.Author = updatedBook.Author
+	book.Year = updatedBook.Year
+
+	if err := db.Save(&book).Error; err != nil {
+		c.IndentedJSON(http.StatusInternalServerError, gin.H{"message" : "failed to update book"})
+	}
+	c.IndentedJSON(http.StatusOK, book)
+
 }
 
 func deleteBook(c *gin.Context){
 	id := c.Param("id")
-	initialLength := len(books)
-
-	books = filterBooks(books, func(book Book) bool{
-		return book.ID != id
-	})
-
-	if len(books) == initialLength {
-		c.IndentedJSON(http.StatusNotFound, gin.H{"message" : "book not found"})
+	if err := db.Delete(&book{}, "id = ?" , id).Error; err != nil {
+		c.IndentedJSON(http.StatusNotFound, gin.H{"message" : "failed to delete book"})
 		return
 	}
 	c.IndentedJSON(http.StatusOK, gin.H{"message" : "book deleted"})
+	
 }
 
-	func filterBooks(input []Book, predicate func(Book) bool) []Book {
-		var result []Book
-		for _, book := range input {
-			if predicate(book) {
-				result = append(result, book)
-			}
-		}
-	return result
-}
+	
